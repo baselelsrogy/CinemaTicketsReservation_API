c5b2707f325c68143d95a880c3248ad377db92b5
baselelsrogy <baselhany88@gmail.com>
2025-04-16 01:23:26 +0200

    create class based-views

--- project/urls.py
+++ project/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     #3.1 GET POST from rest framework function based-views @api_view
     path('rest-fbv/', views.FBV_List),
     #3.2 GET PUT DELETE from rest framework function based-views @api_view
-    path('rest-fbv/<int:pk>/', views.FBV_pk)
+    path('rest-fbv/<int:pk>/', views.FBV_pk),
+    #4.1 GET POST from rest framework class based-view APIview
+    path('rest-cbv/', views.CBV_List.as_view()),
+    #4.2 GET PUT DELETE from rest framework class based-view APIview
+    path('rest-cbv/<int:pk>', views.CBV_pk.as_view())
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -5,6 +5,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import MovieSerializer, ReservationSerializer, GuestSerializer
 from rest_framework import status, filters
+from rest_framework.views import APIView
+from django.http import Http404
+# from rest_framework import generics, mixins
 
 # Create your views here.
 
@@ -81,6 +84,47 @@ def FBV_pk(request, pk):
             return Response(seriaizer.data, status=status.HTTP_200_OK)
         return Response(seriaizer.errors, status=status.HTTP_400_BAD_REQUEST)
     # DELETE
-    elif request.method == 'PUT':
+    elif request.method == 'DELETE':
         guest.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# CBV class based-view
+#4.1 List and Create == GET POST
+class CBV_List(APIView):
+    def get(self, request):
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = GuestSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+#4.2 GET PUT DELETE class based-views -> pk
+class CBV_pk(APIView):
+    def get_object(self, pk):
+        try:
+            return Guest.objects.get(pk=pk)
+        except Guest.DoesNotExist:
+            raise Http404
+    # GET
+    def get(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data)
+    # PUT
+    def put(self, request, pk):
+        guest = self.get_object(pk)
+        serializer = GuestSerializer(guest, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # DELETE
+    def delete(self, request, pk):
+        guest = self.get_object(pk)
+        guest.delete()
+        
